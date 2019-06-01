de5f78d7ba1029eddab706088ac8fecb9bf0b2b3
Davinderpro01 <47469466+Davinderpro01@users.noreply.github.com>
2019-06-01 16:46:29 -0600

    final commit

--- examen.py
+++ examen.py
@@ -2,26 +2,38 @@
 #Semestre: primer semestre
 #Nombre: Devin Alexander Solórzano Morales
 #Carné: 0907-19-8968
+
 #librería para interfaz gráfica
 from tkinter import *
 
 # esta es la función que ejecuta las operaciones del primer botón
 def operacion1():
-    if int(Vartexto1.get()) < int(Vartexto3.get()):
-        VarResultado.set("El resultado de la multiplicación es = " + str(int(Vartexto1.get()) * int(Vartexto2.get()) * int(Vartexto3.get())))    
+    if int(Var1.get()) < int(Var3.get()):
+        VarResultado.set("El resultado de la multiplicación es = " + str(int(Var1.get()) * int(Var2.get()) * int(Var3.get())))    
     else:    
-        if int(Vartexto1.get()) == int(Vartexto3.get()):
-            VarResultado.set("El resultado de la suma es = " + str(int(Vartexto1.get()) + int(Vartexto2.get()) + int(Vartexto3.get())))
+        if int(Var1.get()) == int(Var3.get()):
+            VarResultado.set("El resultado de la suma es = " + str(int(Var1.get()) + int(Var2.get()) + int(Var3.get())))
         else:    
-            if int(Vartexto2.get()) == 0:
-                if int(Vartexto1.get()) > int(Vartexto3.get()):
-                    VarResultado.set("El resultado de la resta es = " + str(int(Vartexto1.get()) - int(Vartexto3.get())))
+            if int(Var2.get()) == 0:
+                if int(Var1.get()) > int(Var3.get()):
+                    VarResultado.set("El resultado de la resta es = " + str(int(Var1.get()) - int(Var3.get())))
                 else:
-                    VarResultado.set("El resultado de la resta es = " + str(int(Vartexto3.get()) - int(Vartexto1.get())))
+                    VarResultado.set("El resultado de la resta es = " + str(int(Var3.get()) - int(Var1.get())))
 
 # esta es la función que ejecuta las operaciones del primer botón
 def operacion2():
-    VarResultado.set("el texto concatenado es = " + str((Vartexto1.get()) + (Vartexto2.get()) + (Vartexto3.get())))
+    Valor_1 = int(Var1.get())
+    Valor_2 = int(Var2.get())
+    Valor_3 = int(Var3.get())
+    Valoperacion = (Valor_1*Valor_2+Valor_3)
+    concatenacionfor= str(Valor_1) + str(Valor_2) + str(Valor_3)
+    Devinimpresion = ""
+    
+    #operación del bucle for
+    for x in range(Valoperacion):
+        Devinimpresion = Devinimpresion + concatenacionfor
+    
+    VarResultado.set(Devinimpresion) 
 
 #para crear la ventana
 Devin = Tk()
@@ -37,29 +49,29 @@ Alexander = LabelFrame(Devin, text = "operaciones")
 Alexander.pack()
 
 #input1
-texto1 = Label(Alexander, text = 'Primer numero: ').grid(row = 1, column = 0)
-Vartexto1 = StringVar()
-cuadro1 = Entry(Alexander, textvariable= Vartexto1).grid(row = 1, column = 1, padx = 50, pady = 20)
+mensaje1 = Label(Alexander, text = 'Primer numero: ').grid(row = 1, column = 0)
+Var1 = StringVar()
+ingreso1 = Entry(Alexander, textvariable= Var1).grid(row = 1, column = 1, padx = 50, pady = 20)
 
 # input2
-texto2 =Label(Alexander, text = 'Segundo numero: ').grid(row = 2, column = 0)
-Vartexto2 = StringVar()
-cuadro2 = Entry(Alexander, textvariable= Vartexto2).grid(row = 2, column = 1, padx = 50, pady = 10)
+mensaje2 =Label(Alexander, text = 'Segundo numero: ').grid(row = 2, column = 0)
+Var2 = StringVar()
+ingreso2 = Entry(Alexander, textvariable= Var2).grid(row = 2, column = 1, padx = 50, pady = 10)
 
 # input3
-texto3 =Label(Alexander, text = 'Tercer numero: ').grid(row = 3, column = 0)
-Vartexto3 = StringVar()
-cuadro3 = Entry(Alexander, textvariable= Vartexto3).grid(row = 3, column = 1, padx = 50, pady = 20)
+mensaje3 =Label(Alexander, text = 'Tercer numero: ').grid(row = 3, column = 0)
+Var3 = StringVar()
+ingreso4 = Entry(Alexander, textvariable= Var3).grid(row = 3, column = 1, padx = 50, pady = 20)
 
 #para crear el primer botón de opciones
-Boton = Button(Alexander, text = "Botón 1", command = operacion1).grid(row = 4, columnspan = 2, pady = 10)
+Botoncito = Button(Alexander, text = "Botón 1", command = operacion1).grid(row = 4, columnspan = 2, pady = 10)
 
 #para crear el segundo botón de opciones
-Boton2 = Button(Alexander, text = "Botón 2", command = operacion2).grid(row = 5, columnspan = 2, pady = 10)
+Botoncito2 = Button(Alexander, text = "Botón 2", command = operacion2).grid(row = 5, columnspan = 2, pady = 10)
 
 #para crear la pestaña en donde va a aparecer el resultado
 VarResultado = StringVar()
-Cuadro4 = Entry(Alexander, textvariable=VarResultado, justify = "center").grid(row = 6, columnspan = 2, pady = 10, sticky = W + E)
+cuadro = Entry(Alexander, textvariable=VarResultado, justify = "center").grid(row = 6, columnspan = 2, pady = 10, sticky = W + E)
 
 #para crear oficialmente la ventana
 Devin.mainloop()
